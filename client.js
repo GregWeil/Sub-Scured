bdcc3f400852e56e3165f7616c1fff86883ef478
Glitch (glitch-blank-node) <none>
2022-10-01 02:07:46 +0000

    💏🐟 Checkpoint ./client.js:97325/8782

--- client.js
+++ client.js
@@ -1,16 +1,39 @@
-import {OrthographicCamera,Scene,WebGLRenderer,BoxGeometry,MeshNormalMaterial,Mesh} from 'three';
+import {
+  OrthographicCamera,
+  Scene,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshNormalMaterial,
+  Mesh,
+} from "three";
 
-const camera = new OrthographicCamera(-100,100,-100,100,1,1000)
+const camera = new OrthographicCamera(-100, 100, -100, 100, 1, 1000);
+camera.position.z = 1;
 
-const scene = new Scene()
+const scene = new Scene();
+const geometry = new BoxGeometry(10, 10, 10);
+const material = new MeshNormalMaterial();
+const mesh = new Mesh(geometry, material);
+scene.add(mesh);
 
-const geometry = new BoxGeometry(0.2,0.2,0.2);
-const material=new MeshNormalMaterial()
-const mesh = new Mesh(geometry,material)
-scene.add(mesh)
-
-const renderer=new WebGLRenderer({antialias:true})
-renderer.setSize(window.innerWidth,window.innerHeight);
+const renderer = new WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-console.log('Hello world!');
+const resize = () => {
+  const scale = (100 * 100) / (window.innerWidth * window.innerHeight);
+  camera.left = (-scale * window.innerWidth) / 2;
+  camera.right = (-scale * window.innerWidth) / 2;
+  camera.top = (-scale * window.innerHeight) / 2;
+  camera.bottom = (-scale * window.innerHeight) / 2;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
+window.addEventListener("resize", resize);
+resize();
+
+renderer.setAnimationLoop((time) => {
+  renderer.render(scene, camera);
+});
+
+console.log("Hello world!");
