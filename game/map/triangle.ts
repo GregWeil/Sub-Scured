55c28e82116b1e00da811d053e25762eda22a426
Glitch (glitch-blank-node) <none>
2022-10-01 14:18:42 +0000

    🎬🍎 Checkpoint ./game/map/triangle.ts:97325/195 ./util/collide.ts:97325/301

---
+++ game/map/triangle.ts
@@ -0,0 +1,8 @@
+import {Vector2}from 'three'
+
+export default class Triangle {
+  private x: number;
+private y:number;
+  
+  constructor(x:number,y:number, radius:number)
+}
---
+++ util/collide.ts
@@ -0,0 +1,8 @@
+import {intersection,findIntersection}from 'segment-intersection';
+import {Vector2}from 'three';
+
+type Segment = [number,number,number,number];
+
+export const intersections(a: Segment,b:Segment)=>{
+  
+}
