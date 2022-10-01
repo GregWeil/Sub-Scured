b993a8faf35994ab662b07444304ab15c6e9c351
Glitch (glitch-blank-node) <none>
2022-10-01 01:55:44 +0000

    🍵⛄️ Checkpoint ./public/index.html:97325/655 ./client.js:97325/646 ./package.json:97325/1286 ./public/style.css:97325/149

--- client.js
+++ client.js
@@ -1,1 +1,16 @@
-console.log("hello world!");
+import {OrthographicCamera,Scene,WebGLRenderer,BoxGeometry,MeshNormalMaterial,Mesh} from 'three';
+
+const camera = new OrthographicCamera(-100,100,-100,100,1,1000)
+
+const scene = new Scene()
+
+const geometry = new BoxGeometry(0.2,0.2,0.2);
+const material=new MeshNormalMaterial()
+const mesh = new Mesh(geometry,material)
+scene.add(mesh)
+
+const renderer=new WebGLRenderer({antialias:true})
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+console.log('Hello world!');
