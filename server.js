17ef3e04530910a315208ce089ccc79f22469b05
Glitch (glitch-blank-node) <none>
2022-10-01 01:08:35 +0000

    🐈💼 Checkpoint ./package.json:97325/86 ./server.js:97325/761 ./public/index.html:97325/1917 ./client.js:97325/85

--- server.js
+++ server.js
@@ -12,32 +12,6 @@ fastify.register(require("@fastify/static"), {
   prefix: "/", // optional: default '/'
 });
 
-// point-of-view is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
-
-// Our main GET home page route, pulls from src/pages/index.hbs
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = {
-    greeting: "Hello Node!",
-  };
-  // request.query.paramName <-- a querystring example
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-// A POST route to handle form submissions
-fastify.post("/", function (request, reply) {
-  let params = {
-    greeting: "Hello Form!",
-  };
-  // request.body.paramName <-- a form post example
-  return reply.view("/src/pages/index.hbs", params);
-});
-
 // Run the server and report out to the logs
 fastify.listen(
   { port: process.env.PORT, host: "0.0.0.0" },
