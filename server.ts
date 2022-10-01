af7beeb98f4960236dcf7a7c8e2ba1a05d21a536
Glitch (glitch-blank-node) <none>
2022-10-01 13:44:14 +0000

    📺🚵 Checkpoint ./server.ts:97325/4449 ./client.ts:97325/2 ./package.json:97325/1893 ./tsconfig.json:97325/13

--- server.ts
+++ server.ts
@@ -1,28 +1,23 @@
 import { join } from "path";
-import * as fastify from "fastify";
-import * as fastifyStatic from "@fastify/static";
+import fastifyServer from "fastify";
+import fastifyStatic from "@fastify/static";
 
-// Require the fastify framework and instantiate it
-const server = fastify({
-  // set this to true for detailed logging:
-  logger: false,
-});
+const server = fastifyServer({ logger: false });
 
-// Setup our static files
 server.register(fastifyStatic, {
   root: join(process.cwd(), "public"),
   prefix: "/",
 });
+
 server.register(fastifyStatic, {
   root: join(process.cwd(), "build"),
   prefix: "/build",
   decorateReply: false,
 });
 
-// Run the server and report out to the logs
 server.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
+  { port: Number(process.env.PORT), host: "0.0.0.0" },
+  (err, address) => {
     if (err) {
       console.error(err);
       process.exit(1);
